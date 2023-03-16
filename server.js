b571a1ec9f7ff473b93d760f18bbb24de7de9f62
Georgefdz <georgfdz@gmail.com>
2023-03-16 12:19:33 -0600

    trying some changes

--- server.js
+++ server.js
@@ -9,11 +9,14 @@ let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
-    })
+
+
+
+    // MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+//     .then(client => {
+//         console.log(`Connected to ${dbName} Database`)
+//         db = client.db(dbName)
+//     })
     
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -89,6 +92,18 @@ app.delete('/deleteItem', (request, response) => {
 
 })
 
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+async function init() {
+    try {
+      const client = await MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true });
+      console.log(`Connected to ${dbName} Database`);
+      db = client.db(dbName);
+  
+      app.listen(process.env.PORT || PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
+    } catch (err) {
+      console.error('Failed to connect to the database:', err);
+    }
+  }
+  
+  init();
